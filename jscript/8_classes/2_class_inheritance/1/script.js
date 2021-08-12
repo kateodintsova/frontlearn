dc0153835a77f9fee54e948e58cbb102e5535ebd
kateodintsova <https://github.com/kateodintsova>
2021-08-13 00:52:32 +0500

    пройдено несколько уроков 8 главы Классы

--- jscript/8_classes/2_class_inheritance/1/script.js
+++ jscript/8_classes/2_class_inheritance/1/script.js
@@ -1,1 +1,18 @@
-﻿
+﻿class Animal {
+
+    constructor(name) {
+        this.name = name;
+    }
+
+}
+
+class Rabbit extends Animal {
+    constructor(name) {
+        super();
+        this.name = name;
+        this.created = Date.now();
+    }
+}
+
+let rabbit = new Rabbit("Белый кролик");
+alert(rabbit.name);
---
+++ jscript/8_classes/4_private_protected_properties_methods/2/script.js
@@ -0,0 +1,14 @@
+﻿class CoffeeMachine {
+    #waterLimit = 200; // приватное свойство
+
+    #checkWater(value) {
+        if (value < 0) throw new Error("Отрицательный уровень воды");
+        if (value > this.#waterLimit) throw new Error("Слишком много воды");
+    }
+}
+
+let coffeeMachine = new CoffeeMachine();
+
+// снаружи  нет доступа к приватным методам класса
+coffeeMachine.#checkWater(); // Error
+coffeeMachine.#waterLimit = 1000; // Error
