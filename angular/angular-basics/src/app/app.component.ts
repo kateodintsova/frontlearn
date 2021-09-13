77f3ba5a74a95445440f77aa556535bf116066fa
kateodintsova <kate.odintsova@gmail.com>
2021-09-13 23:33:44 +0500

    ngFor

--- angular/angular-basics/src/app/app.component.ts
+++ angular/angular-basics/src/app/app.component.ts
@@ -6,5 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  toggle: any = false;
+  arr = [1, 1, 2, 3, 5, 8, 13];
+
+  objs = [
+    {
+      title: 'Post 1',
+      author: 'Kate',
+      comments: [{
+        name: 'Max',
+        text: 'lorem 1'
+      },
+      {
+        name: 'Max',
+        text: 'lorem 2'
+      },
+      {
+        name: 'Max',
+        text: 'lorem 3'
+      }] 
+    },
+    {
+      title: 'Post 2',
+      author: 'Kate',
+      comments: [{
+        name: 'Max 2',
+        text: 'lorem 1'
+      },
+      {
+        name: 'Max 2',
+        text: 'lorem 2'
+      },
+      {
+        name: 'Max 2',
+        text: 'lorem 3'
+      }] 
+    }
+  ]
 }
