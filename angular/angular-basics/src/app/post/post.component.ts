97ba46251544fd0882b7055e2747f14d11404cc1
kateodintsova <kate.odintsova@gmail.com>
2021-09-23 16:01:31 +0500

    lesson 26. Доступ до контента с ContentChild

--- angular/angular-basics/src/app/post/post.component.ts
+++ angular/angular-basics/src/app/post/post.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ContentChild, ElementRef } from '@angular/core';
 import { Post } from '../app.component';
 
 @Component({
@@ -9,10 +9,12 @@ import { Post } from '../app.component';
 export class PostComponent implements OnInit {
 
   @Input() post: Post;
+  @ContentChild('info', {static: true}) infoRef: ElementRef;
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.infoRef.nativeElement);
   }
 
 }
