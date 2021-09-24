5b4451482f65f0d84818c94b71c87d9025351a59
kateodintsova <kate.odintsova@gmail.com>
2021-09-24 23:43:26 +0500

    lesson 35. Создание структурной директивы

--- angular/angular-basics/src/app/app.module.ts
+++ angular/angular-basics/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { StyleDirective } from './directives/style.directive';
+import { IfnotDirective } from './directives/ifnot.directive';
 @NgModule({
   declarations: [
     AppComponent,
     StyleDirective,
+    IfnotDirective,
   ],
   imports: [
     BrowserModule,
