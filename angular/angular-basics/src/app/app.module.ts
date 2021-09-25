a55a6af05d211bd9156e0d1c79e3985ff52d147b
kateodintsova <kate.odintsova@gmail.com>
2021-09-25 12:35:02 +0500

    lesson 36. Работа с числами

--- angular/angular-basics/src/app/app.module.ts
+++ angular/angular-basics/src/app/app.module.ts
@@ -3,13 +3,9 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { StyleDirective } from './directives/style.directive';
-import { IfnotDirective } from './directives/ifnot.directive';
 @NgModule({
   declarations: [
     AppComponent,
-    StyleDirective,
-    IfnotDirective,
   ],
   imports: [
     BrowserModule,
