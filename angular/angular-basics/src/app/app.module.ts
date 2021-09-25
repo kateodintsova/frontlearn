8505caf011aa087151870cd2f35c1a1bb56b6720
kateodintsova <kate.odintsova@gmail.com>
2021-09-25 13:03:30 +0500

    lesson 41. Создание своего пайпа

--- angular/angular-basics/src/app/app.module.ts
+++ angular/angular-basics/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { MultByPipe } from './pipes/mult-by.pipe';
 
 import { AppComponent } from './app.component';
 @NgModule({
   declarations: [
     AppComponent,
+    MultByPipe,
   ],
   imports: [
     BrowserModule,
