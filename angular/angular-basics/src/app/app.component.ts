208c25ffe3a3d0da84dfd0d5fe44aba52ab51594
kateodintsova <kate.odintsova@gmail.com>
2021-09-25 12:50:43 +0500

    lesson 40. Еще пайпы

--- angular/angular-basics/src/app/app.component.ts
+++ angular/angular-basics/src/app/app.component.ts
@@ -10,4 +10,16 @@ export class AppComponent {
   e: number = Math.E;
   str: string = 'hello world';
   date: Date = new Date();
+  float: number = 0.42;
+
+  obj = {
+    a: 1,
+    b: {
+      c: 2,
+      d: {
+        e: 3,
+        f: 4
+      }
+    }
+  }
 }
