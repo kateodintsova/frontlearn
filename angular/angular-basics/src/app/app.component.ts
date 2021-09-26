410060ec8b158774324f5557cc20e42d9fa57fe7
kateodintsova <kate.odintsova@gmail.com>
2021-09-26 22:50:51 +0500

    lesson 49. Область видимости сервисов

--- angular/angular-basics/src/app/app.component.ts
+++ angular/angular-basics/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AppCounterService } from './services/app-counter.service';
 import { LocalCounterService } from './services/local-counter.service';
+import { CounterComponent } from './counter/counter.component';
 
 @Component({
   selector: 'app-root',
--- angular/angular-basics/src/app/app.module.ts
+++ angular/angular-basics/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { CounterComponent } from './counter/counter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    CounterComponent,
   ],
   imports: [
     BrowserModule,
